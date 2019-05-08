ed411b652c41c0378fd14891456ad83ae4282dac
Martin Darmüntzel <martin@trivialanalog.de>
2019-05-08 12:14:36 +0200

    Erweitert den Adminbereich.

--- fsmedhro_core/admin.py
+++ fsmedhro_core/admin.py
@@ -8,13 +8,31 @@ from .models import (
 )
 
 
+class FachschaftUserAdmin(admin.ModelAdmin):
+    model = FachschaftUser
+    fields = [
+        'nickname',
+        'user.first_name',
+        'user.last_name',
+        'gender',
+        'studiengang',
+        'studienabschnitt',
+    ]
+
+
+class GenderAdmin(admin.ModelAdmin):
+    model = Gender
+    fields = ['bezeichnung', 'endung']
+
+
 class StudiengangAdmin(admin.ModelAdmin):
     model = Studiengang
+    fields = ['bezeichnung']
     # If you don't specify this, you will get a multiple select widget:
     filter_horizontal = ('studienabschnitt',)
 
 
-admin.site.register(FachschaftUser)
-admin.site.register(Gender)
+admin.site.register(FachschaftUser, FachschaftUserAdmin)
+admin.site.register(Gender, GenderAdmin)
 admin.site.register(Studienabschnitt)
 admin.site.register(Studiengang, StudiengangAdmin)
