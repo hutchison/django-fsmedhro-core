89c24745ef5b5de639d04c2100fd1e432de850de
Martin Darmüntzel <martin@trivialanalog.de>
2019-05-08 12:25:04 +0200

    isort forms.py

--- fsmedhro_core/forms.py
+++ fsmedhro_core/forms.py
@@ -1,6 +1,7 @@
-from .models import FachschaftUser
-from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+from .models import FachschaftUser
 
 
 class UserForm(ModelForm):
