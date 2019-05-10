ee96426635e3e84ced298fe8293f58096035f814
Martin Darmüntzel <martin@trivialanalog.de>
2019-05-10 21:07:37 +0200

    Entfernt überflüssige path-Einträge in urls.py.

--- fsmedhro_core/urls.py
+++ fsmedhro_core/urls.py
@@ -6,6 +6,4 @@ app_name = 'fsmedhro_core'
 urlpatterns = [
     path('edit/', views.FachschaftUserEdit.as_view(), name='edit'),
     path('', views.FachschaftUserDetail.as_view(), name='detail'),
-    # path('user/<int:user_id>', views.user_profile, name='user_profile'),
-    # path('user', views.user_self, name='user_self'),
 ]
