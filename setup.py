8f2cf70fac8489ce49f77bf16c9608cdd9b0c286
Martin Darmüntzel <martin@trivialanalog.de>
2019-05-13 13:51:00 +0200

    Fügt ldap3 als Abhängigkeit hinzu.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-fsmedhro-core',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(),
     include_package_data=True,
     description='Basisapp für die Webseite des FSR Medizin der Uni Rostock',
@@ -19,6 +19,7 @@ setup(
     install_requires=[
         'Django>=2.1',
         'django-crispy-forms',
+        'ldap3>=2.6',
     ],
     python_requires=">=3.5",
     author='Martin Darmüntzel',
