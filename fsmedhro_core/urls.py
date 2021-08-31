c895d56605e58c88d63e55ed6733c13a3206666a
Martin Darmüntzel <martin@trivialanalog.de>
2021-08-31 08:57:13 +0200

    Fügt Rundmail hinzu.

--- fsmedhro_core/urls.py
+++ fsmedhro_core/urls.py
@@ -5,5 +5,6 @@ app_name = 'fsmedhro_core'
 
 urlpatterns = [
     path('edit/', views.FachschaftUserEdit.as_view(), name='edit'),
+    path('rundmail/', views.Rundmail.as_view(), name='rundmail'),
     path('', views.FachschaftUserDetail.as_view(), name='detail'),
 ]
