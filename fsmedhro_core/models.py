a51a98e96c8c3d2d8542c258d1615eb3ada71680
Martin Darmüntzel <martin@trivialanalog.de>
2024-03-04 08:19:16 +0100

    Fügt Model Kontaktdaten hinzu.

--- fsmedhro_core/models.py
+++ fsmedhro_core/models.py
@@ -97,3 +97,59 @@ class FachschaftUser(models.Model):
         verbose_name = "Student:in"
         verbose_name_plural = "Student:innen"
         ordering = ["nickname"]
+
+
+class Kontaktdaten(models.Model):
+    """
+    Die Kontaktdaten werden vom fachschaftUser ausgelagert, weil auch historische
+    Kontaktdaten erfasst werden müssen. Wenn jemand die Kontaktdaten verifizieren
+    lässt, dann ein Buch ausleiht und dann die Kontaktdaten ändert, dann weiß niemand
+    mehr, wie das Buch wieder eingetrieben werden kann.
+    """
+    fachschaftuser = models.ForeignKey(
+        FachschaftUser,
+        on_delete=models.CASCADE,
+        related_name="kontaktdaten",
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="erzeugt",
+    )
+    """
+    Die Adresse ist einfach nur ein Textfeld, weil es von Menschen manuell überprüft
+    werden soll. Daher spalten wir das nicht extra auf.
+    """
+    adresse = models.TextField()
+    telefonnummer = models.CharField(
+        max_length=16,
+        default="",
+        validators=[telefon_regex],
+        verbose_name="Telefonnummer",
+        help_text=(
+            "Telefonnummer muss von folgendem Format sein: '+4938149444614'. "
+            "Es sind bis zu 15 Ziffern erlaubt."
+        ),
+    )
+    gecheckt_von = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="überprüft von",
+    )
+    gecheckt_datum = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name="überprüft am",
+    )
+
+    class Meta:
+        verbose_name = "Kontaktdaten"
+        verbose_name_plural = "Kontaktdaten"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return f"{self.fachschaftuser} {self.created}"
+
+    def ist_verifiziert(self):
+        return all([self.gecheckt_von, self.gecheckt_datum])
