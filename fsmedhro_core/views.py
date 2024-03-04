58d123b5665d51ba8ea8d0e6a4555534167d17f1
Martin Darmüntzel <martin@trivialanalog.de>
2024-03-04 08:24:12 +0100

    Fügt alles für Kontaktdaten hinzu.
    Views, URLs, Templates, API.

--- fsmedhro_core/views.py
+++ fsmedhro_core/views.py
@@ -1,67 +1,150 @@
 import logging
 
-from django.conf import settings
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.mail import send_mail, EmailMessage
-from django.shortcuts import redirect, render
-from django.views import View
+from datetime import datetime
 
-from .forms import FachschaftUserForm
-from .models import Studienabschnitt, Studiengang, Gender, FachschaftUser
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.mail import send_mail, EmailMessage
+from django.forms.models import model_to_dict
+from django.shortcuts import redirect, render, get_object_or_404
+from django.views import View
+from django.views.generic import ListView
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from .forms import FachschaftUserForm, KontaktdatenForm
+from .models import Studienabschnitt, Studiengang, Gender, FachschaftUser, Kontaktdaten
 
 
 logger = logging.getLogger(__name__)
 
 
 class FachschaftUserEdit(LoginRequiredMixin, View):
+    template_name = "fsmedhro_core/user_edit.html"
+    form_class = FachschaftUserForm
+
     def get(self, request):
-        try:
-            # FachschaftUser bereits vorhanden?
-            form = FachschaftUserForm(instance=request.user.fachschaftuser)
-        except ObjectDoesNotExist:
-            form = FachschaftUserForm()
+        # FachschaftUser bereits vorhanden?
+        if hasattr(request.user, 'fachschaftuser'):
+            fu_form = self.form_class(instance=request.user.fachschaftuser)
+        else:
+            fu_form = self.form_class()
 
         context = {
-            'form': form,
+            "fu_form": fu_form,
+            "next": request.GET.get(REDIRECT_FIELD_NAME, ""),
         }
 
-        return render(request, 'fsmedhro_core/user_edit.html', context)
+        return render(request, self.template_name, context)
 
     def post(self, request):
-        try:
-            form = FachschaftUserForm(
+        if hasattr(request.user, 'fachschaftuser'):
+            fu_form = self.form_class(
                 data=request.POST,
                 instance=request.user.fachschaftuser,
             )
-        except ObjectDoesNotExist:
-            form = FachschaftUserForm(
+        else:
+            fu_form = self.form_class(
                 data=request.POST,
             )
 
-        if form.is_valid():
-            fachschaftuser = form.save(commit=False)
+        if fu_form.is_valid():
+            fachschaftuser = fu_form.save(commit=False)
             fachschaftuser.user = request.user
             fachschaftuser.save()
 
-        return redirect('fsmedhro_core:detail')
+            # TODO: redirect zu next, falls vorhanden
+
+            return redirect("fsmedhro_core:detail")
+        else:
+            context = {
+                "fu_form": fu_form,
+            }
+            return render(request, self.template_name, context)
 
 
 class FachschaftUserDetail(LoginRequiredMixin, View):
+    template_name = "fsmedhro_core/user_detail.html"
+
     def get(self, request):
-        try:
-            fachschaftuser = request.user.fachschaftuser
-        except ObjectDoesNotExist:
+        if not hasattr(request.user, 'fachschaftuser'):
             return redirect("fsmedhro_core:edit")
+
+        fachschaftuser = request.user.fachschaftuser
 
         context = {
             "fachschaftuser": fachschaftuser,
+            "aktuelle_kontaktdaten": fachschaftuser.kontaktdaten.first(),
+            "kontaktdaten": fachschaftuser.kontaktdaten.all(),
         }
 
-        return render(request, 'fsmedhro_core/user_detail.html', context)
+        return render(request, self.template_name, context)
+
+
+class KontaktdatenList(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    queryset = Kontaktdaten.objects.select_related('fachschaftuser__user').filter(gecheckt_von=None)
+    context_object_name = 'kontaktdaten'
+    template_name = 'fsmedhro_core/kontaktdaten_list.html'
+
+    def test_func(self):
+        return self.request.user.has_perm('fsmedhro_core.change_kontaktdaten')
+
+
+class KontaktdatenVerify(LoginRequiredMixin, UserPassesTestMixin, View):
+    def test_func(self):
+        return self.request.user.has_perm('fsmedhro_core.change_kontaktdaten')
+
+    def post(self, request):
+        print(request.POST)
+        k_id = request.POST.get('kontaktdaten_id', None)
+        k = get_object_or_404(Kontaktdaten, pk=k_id)
+
+        k.gecheckt_von = request.user
+        k.gecheckt_datum = datetime.now()
+        k.save()
+
+        return redirect('fsmedhro_core:kontaktdaten_list')
+
+
+class KontaktdatenAdd(LoginRequiredMixin, View):
+    template_name = "fsmedhro_core/kontaktdaten_add.html"
+    form_class = KontaktdatenForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if not hasattr(request.user, 'fachschaftuser'):
+            return redirect("fsmedhro_core:edit")
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        context = {
+            "kontaktdaten_form": self.form_class(),
+        }
+
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        kontaktdaten_form = self.form_class(data=request.POST)
+
+        if kontaktdaten_form.is_valid():
+            kontaktdaten = kontaktdaten_form.save(commit=False)
+            kontaktdaten.save()
+
+            return redirect("fsmedhro_core:detail")
+        else:
+            context = {
+                "kontaktdaten_form": kontaktdaten_form,
+            }
+
+            return render(request, self.template_name, context)
 
 
 class Rundmail(UserPassesTestMixin, View):
+    template_name = "fsmedhro_core/rundmail.html"
+
     def test_func(self):
         return self.request.user.is_staff
 
@@ -75,7 +158,7 @@ class Rundmail(UserPassesTestMixin, View):
 
     def get(self, request):
         context = self.get_context_data()
-        return render(request, 'fsmedhro_core/rundmail.html', context)
+        return render(request, self.template_name, context)
 
     def post(self, request):
         context = self.get_context_data()
@@ -127,7 +210,7 @@ class Rundmail(UserPassesTestMixin, View):
             context["testmail_verschickt"] = True
             context["anzahl_verschickt"] = anzahl_verschickt
 
-            return render(request, 'fsmedhro_core/rundmail.html', context)
+            return render(request, self.template_name, context)
         else:
             empfaenger = FachschaftUser.objects.filter(
                 studiengang__in=studiengaenge,
@@ -170,4 +253,4 @@ class Rundmail(UserPassesTestMixin, View):
                 fail_silently=False,
             )
 
-            return render(request, 'fsmedhro_core/rundmail.html', context)
+            return render(request, self.template_name, context)
